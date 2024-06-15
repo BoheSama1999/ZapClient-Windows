ca1596c801d0672d2d319f74d49e01ebc664481a
BoheSama1999 <ZhiboheMain@gmail.com>
2024-06-15 20:39:48 +0800

    fix imgui include

--- imgui/imgui_impl_glfw.cpp
+++ imgui/imgui_impl_glfw.cpp
@@ -69,16 +69,16 @@
 #endif
 
 // GLFW
-#include <GLFW/glfw3.h>
+#include <glfw3.h>
 
 #ifdef _WIN32
 #undef APIENTRY
 #define GLFW_EXPOSE_NATIVE_WIN32
-#include <GLFW/glfw3native.h>   // for glfwGetWin32Window()
+#include <glfw3native.h>   // for glfwGetWin32Window()
 #endif
 #ifdef __APPLE__
 #define GLFW_EXPOSE_NATIVE_COCOA
-#include <GLFW/glfw3native.h>   // for glfwGetCocoaWindow()
+#include <glfw3native.h>   // for glfwGetCocoaWindow()
 #endif
 
 #ifdef __EMSCRIPTEN__
