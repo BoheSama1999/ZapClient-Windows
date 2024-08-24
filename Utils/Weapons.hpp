0154265bc2d5c03f978be469e0bdaea68d6f2ab1
Zhibohe <153600213+BoheSama1999@users.noreply.github.com>
2024-08-24 22:15:55 +0800

    Update Weapons.hpp

--- Utils/Weapons.hpp
+++ Utils/Weapons.hpp
@@ -2,51 +2,51 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-
+     
 namespace WeaponIDs {
     //Weapon Ids
     //Light
-    int P2020 = 111;
+    int P2020 = 112;
     int RE45 = 85;
     int ALTERNATOR = 84;
-    int R99 = 110;
     int R301 = 0;
-    int SPITFIRE = 112;
+    int SPITFIRE = 113;
     int G7 = 95;
-
+     
     //Heavy
     int FLATLINE = 94;
     int HEMLOCK = 96;
     int PROWLER = 107;
-    int REPEATER = 117;
+    int REPEATER = 118;
     int RAMPAGE = 6;
-    int CAR = 118;
-
+    int CAR = 119;
+     
     //Energy
     int HAVOC = 91;
     int LSTAR = 99;
-    int TRIPLETAKE = 113;
-    int VOLT = 116;
-    int NEMESIS = 119;
-
+    int TRIPLETAKE = 114;
+    int VOLT = 117;
+    int NEMESIS = 120;
+     
     //Shotgun
     int MOZAMBIQUE = 102;
+    int EVA8 = 92;
     int PEACEKEEPER = 109;
     int MASTIFF = 101;
-
+     
     //Sniper
-    int WINGMAN = 114;
+    int WINGMAN = 115;
     int LONGBOW = 90;
     int CHARGE_RIFLE = 87;
     int SENTINEL = 1;
-
+     
     //Legendary
-    int EVA8 = 93;
+    int R99 = 111;
     int DEVOTION = 89;
     int BOCEK = 2;
     int KRABER = 98;
-    int KNIFE = 173;
-
+    int KNIFE = 176;
+     
     //Melee
-    int HANDS = 116;
+    int HANDS = 121;
 };
